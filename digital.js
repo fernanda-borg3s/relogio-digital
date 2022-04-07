ac6c3881093f0334cdb5db4d093d23eb3071d67b
Fernanda Borges <91858227+fernanda-borg3s@users.noreply.github.com>
2022-04-06 21:52:14 -0300

    Add files via upload

---
+++ digital.js
@@ -0,0 +1,35 @@
+function showTime(){
+    let date = new Date();
+    let hora = date.getHours(); //0-23
+    let minutos = date.getMinutes();//0-59
+    let segundos = date.getSeconds();//0-59
+
+    let fundo = background(hora);
+
+// ajustar para os zero a esquerda aparecer
+        if(hora < 10){
+            hora = "0" + hora;
+    }
+        else if(minutos < 10){
+            minutos = "0" + minutos;
+    }
+        else if(segundos < 10){
+            segundos = "0" + segundos;
+    }
+        document.getElementById('relogio').innerHTML = `${hora} : ${minutos} : ${segundos}`
+    }
+setInterval(showTime,1000);
+
+function background(time){
+     if(time >= 0 && time < 12){
+        document.body.style.background = "url(img/manha.jpg) no-repeat center/100% 100% ";
+ } else if (time >= 12 && time < 18){
+    document.body.style.background = "url(img/tarde.jpg) no-repeat center/100% 100% ";
+ }else{
+    document.body.style.background = "url(img/noite.jpg) no-repeat center/100% 100% ";
+ }
+}
+
+  
+
+    
