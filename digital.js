54e4995489b679e220233e972df7a37a3d3da40b
Fernanda Borges <91858227+fernanda-borg3s@users.noreply.github.com>
2022-04-06 21:53:03 -0300

    Update digital.js

--- digital.js
+++ digital.js
@@ -22,11 +22,11 @@ setInterval(showTime,1000);
 
 function background(time){
      if(time >= 0 && time < 12){
-        document.body.style.background = "url(img/manha.jpg) no-repeat center/100% 100% ";
+        document.body.style.background = "url(manha.jpg) no-repeat center/100% 100% ";
  } else if (time >= 12 && time < 18){
-    document.body.style.background = "url(img/tarde.jpg) no-repeat center/100% 100% ";
+    document.body.style.background = "url(tarde.jpg) no-repeat center/100% 100% ";
  }else{
-    document.body.style.background = "url(img/noite.jpg) no-repeat center/100% 100% ";
+    document.body.style.background = "url(noite.jpg) no-repeat center/100% 100% ";
  }
 }
 
